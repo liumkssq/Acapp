e698c622d6bb310e5440677c9fe27ae604981e74
mkssq liu <2591564105mk@gmail.com>
2023-08-19 22:40:06 +0800

    add button

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Acapp
 a game by django
+[术士之战](http://1.117.167.220:8000)
--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -3,8 +3,25 @@ class AcGameMenu {
         this.root = root;
         this.$menu = $(`
         <div class="ac-game-menu">
+            <div class="ac-game-menu-field">
+                <div class="ac-game-menu-field-item ac-game-menu-field-item-single">
+                    单人模式
+                </div>
+                <br>
+                 <div class="ac-game-menu-field-item ac-game-menu-field-item-multi">
+                    多人模式
+                </div>
+                <br>
+                <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+                    设置
+                </div>
+                <br>
+            </div>
         </div>
 `);
       this.root.$ac_game.append(this.$menu);
+      this.$single = this.$menu.find('.ac-game-menu-field-item-single');
+      this.$multi  = this.$menu.find('.ac-game-menu-field-item-multi');
+      this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
     };
 }
