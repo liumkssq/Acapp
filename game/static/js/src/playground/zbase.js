3c4596b366f0521498f81e7cb05d5d6c7ed80cd3
mkssq liu <2591564105mk@gmail.com>
2023-08-19 23:21:05 +0800

    debug

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -2,16 +2,16 @@ class AcGamePlayground {
     constructor(root) {
         this.root = root;
         this.$playground = $(`<div>游戏界面</div>`);
+        this.hide();
         this.root.$ac_game.append(this.$playground);
-        this.show();
+        this.start();
     }
     start() {
-
     }
     show() {
         this.$playground.show();
     }
     hide() {
-        ths.$playground.hide();
+        this.$playground.hide();
     }
 }
