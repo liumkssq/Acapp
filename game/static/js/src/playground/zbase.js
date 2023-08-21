b5cbba5d85b104deb6f1deab9ac11aac9f2db5fb
mkssq liu <2591564105mk@gmail.com>
2023-08-21 20:07:52 +0800

    debug

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -2,16 +2,8 @@ class AcGamePlayground {
     constructor(root) {
         this.root = root;
         this.$playground = $(`<div class="ac-game-playground"></div>`);
-      //  this.hide();
-        this.root.$ac_game.append(this.$playground);
-        this.width = this.$playground.width();
-        this.height = this.$playground.height();
-        this.game_map = new GameMap(this);
-        this.players = [];
-        this.players.push(new Player(this,this.width / 2,this.height / 2, this.height * 0.05,"purple",this.height * 0.15,true));
-        for(let i = 0; i < 5;i++) {
-            this.players.push(new Player(this,this.width / 2,this.height / 2, this.height * 0.05,this.get_radom_color(),this.height * 0.15,false));
-        }
+        this.hide();
+
         this.start();
     }
     get_radom_color() {
@@ -22,6 +14,15 @@ class AcGamePlayground {
     }
     show() {
         this.$playground.show();
+        this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this,this.width / 2,this.height / 2, this.height * 0.05,"purple",this.height * 0.15,true));
+        for(let i = 0; i < 5;i++) {
+            this.players.push(new Player(this,this.width / 2,this.height / 2, this.height * 0.05,this.get_radom_color(),this.height * 0.15,false));
+        }
     }
     hide() {
         this.$playground.hide();
